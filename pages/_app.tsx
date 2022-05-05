7ec5b391d1c06eef0617e96e7d38c0ac4e338381
Devin Santamaria <devin@webprism.co>
2022-05-05 14:11:01 -0700

    move fb pixel to local

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,9 @@ import themeConfig from "../lib/theme";
 import MailingList from "../components/MailingList";
 import "@fontsource/raleway/400.css";
 import "../styles/globals.css";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as fbq from '../lib/fpixel'
 
 const NavBar = dynamic(() => import("../components/NavBar"));
 const Footer = dynamic(() => import("../components/Footer"));
@@ -33,19 +36,20 @@ if (process.env.NODE_ENV === "production" && process.browser) {
   console.log("GTM in production");
   TagManager.initialize(tagManagerArgs);
 }
-if (process.env.NODE_ENV === "development" && process.browser) {
-  console.log("GTM in development");
-  TagManager.initialize(tagManagerArgs);
-}
+// if (process.env.NODE_ENV === "development" && process.browser) {
+//   console.log("GTM in development");
+//   TagManager.initialize(tagManagerArgs);
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const [cart, setCart] = useState<any>({ id: null, lines: [] });
   const shop = {
     name: "TOR Salon Products",
   };
 
   useEffect(() => {
-    if (process.browser) {
+    if (typeof window) {
       window.Tawk_API = window.Tawk_API || {};
       window.Tawk_API.customStyle = {
         zIndex: 1000,
@@ -53,13 +57,30 @@ function MyApp({ Component, pageProps }: AppProps) {
     }
   }, []);
 
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
+
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ChakraProvider theme={customTheme}>
       <ShopContext.Provider value={{ shop }}>
         <Head>
           <meta name="theme-color" content="#ffffff" />
           <link rel="shortcut icon" href="/favicon.png" />
-          <meta name="facebook-domain-verification" content="bk02y72cdwvcwzina508gmb7xv87g6" />
+          <meta
+            name="facebook-domain-verification"
+            content="bk02y72cdwvcwzina508gmb7xv87g6"
+          />
         </Head>
         <CartContext.Provider value={{ cart, setCart }}>
           <NavBar />
@@ -78,6 +99,31 @@ function MyApp({ Component, pageProps }: AppProps) {
           <MailingList />
         </>
       )}
+      <Script
+        id="fb_pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${fbq.FB_PIXEL_ID}');
+          fbq('track', 'PageView');`,
+        }}
+      />
+      <noscript>
+        {/* eslint-disable-next-line @next/next/no-img-element */} {/* eslint-disable-next-line jsx-a11y/alt-text */}
+        <img
+          height="1"
+          width="1"
+          style={{display: 'none' }}
+          src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1"
+        />
+      </noscript>
     </ChakraProvider>
   );
 }
