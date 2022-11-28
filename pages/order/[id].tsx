73d92c6522171ac4b22cb450190eb8be46cb905a
Devin Santamaria <devin@webprism.co>
2022-11-28 09:16:02 -0800

    move plausible purchase tracking to Order Status

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -72,8 +72,6 @@ export default function ThankYou() {
       const orderId = window.location.pathname.split("/")[2];
       const orderValue = parseFloat(data.currentTotalPriceSet.shopMoney.amount);
 
-      plausible("Purchase")
-
       const itemsArray = data.lineItems.edges.map((i: LineItemType) => ({
         item_id: i.node.id, //string
         item_name: i.node.name, //string
@@ -303,3 +301,16 @@ function LineItem({ product }: any) {
     </Stack>
   );
 }
+
+// old GTM code from Shopify
+{/* <script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://gtm.torsalonproducts.com/kthlknzv.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MKG7C6H');
+</script>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKG7C6H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) --> */}
