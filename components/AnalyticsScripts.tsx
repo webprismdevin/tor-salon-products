76bd706885fe1c5c5b8e2d4b11926ff18a5b2e15
Devin Santamaria <devin@webprism.co>
2022-12-16 05:03:10 -0800

    fix NODE_ENV

--- components/AnalyticsScripts.tsx
+++ components/AnalyticsScripts.tsx
@@ -1,24 +1,8 @@
 import Script from "next/script";
 
 export default function AnalyticsScripts() {
-  if(process.env.NODE_ENV === "development") return (
+  if(process.env.NODE_ENV === "production") return (
     <>
-      {/* Facebook */}
-      {/* <Script
-        id="facebookPixel"
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '711750850270833');fbq('track', 'PageView');`,
-        }}
-      /> */}
-      {/* <noscript> */}
-        {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
-        {/* <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=711750850270833&ev=PageView&noscript=1"
-        />
-      </noscript> */}
       {/* Cometly */}
       <Script
         src="https://t.cometlytrack.com/e?uid=cb9398-7126-b99785-s"
