186ffb465712da7c6a164b67ea494a1a1f00abb6
Devin Santamaria <devin@webprism.co>
2023-07-07 07:56:08 -0700

    add update-customer api route

--- lib/shopify-client.ts
+++ lib/shopify-client.ts
@@ -1,9 +1,21 @@
 import { createStorefrontClient } from "@shopify/hydrogen-react";
+import { GraphQLClient } from "graphql-request";
 
 const client = createStorefrontClient({
-    publicStorefrontToken: "a37e8b74cb52b6e0609c948c43bb0a5c",
-    storeDomain: 'tor-salon-products.myshopify.com',
-    storefrontApiVersion: process.env.SHOPIFY_API_VERSION!,
-  });
-  export const getStorefrontApiUrl = client.getStorefrontApiUrl;
-  export const getPrivateTokenHeaders = client.getPrivateTokenHeaders;
+  publicStorefrontToken: "a37e8b74cb52b6e0609c948c43bb0a5c",
+  storeDomain: "tor-salon-products.myshopify.com",
+  storefrontApiVersion: process.env.SHOPIFY_API_VERSION!,
+});
+
+export const getStorefrontApiUrl = client.getStorefrontApiUrl;
+export const getPrivateTokenHeaders = client.getPrivateTokenHeaders;
+
+export const adminGraphClient = new GraphQLClient(
+  "https://tor-salon-products.myshopify.com/admin/api/2022-07/graphql.json" as string,
+  {
+    headers: {
+      "X-Shopify-Access-Token": process.env
+        .SHOPIFY_ADMIN_API_PASSWORD as string,
+    },
+  }
+);
---
+++ pages/api/update-customer.ts
@@ -0,0 +1,77 @@
+import { gql } from "graphql-request";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { adminGraphClient } from "lib/shopify-client";
+
+type Data = {};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // get URL params from the request
+  // email, tag and redirect
+  const { em, t, redir } = req.query;
+
+    // get the customer ID from the email
+    const query = gql`
+        query getCustomers($email: String!) {
+            customers(first: 10, query: $email) {
+                edges {
+                    node {
+                        id
+                        email
+                        tags
+                    }
+                }
+            }
+        }
+    `;
+
+    const queryVariables = {
+        email: decodeURIComponent(em as string),
+    };
+
+    const response: any = await adminGraphClient.request(query, queryVariables);
+
+    // if the customer exists, update the tags
+    if (response.customers.edges.length > 0) {
+        const updateMutation = gql`
+            mutation customerUpdate($input: CustomerInput!) {
+                customerUpdate(input: $input) {
+                    customer {
+                        id
+                        firstName
+                        lastName
+                        tags
+                        email
+                        phone
+                        taxExempt
+                        acceptsMarketing
+                    }
+                    userErrors {
+                        field
+                        message
+                    }
+                }
+            }
+        `;
+        const updateVariables = {
+            input: {
+                id: response.customers.edges[0].node.id,
+                email: decodeURIComponent(em as string),
+                tags: [decodeURIComponent(t as string), ...response.customers.edges[0].node.tags],
+            },
+        };
+
+        const updateResponse: any = await adminGraphClient.request(
+            updateMutation,
+            updateVariables
+        );
+
+        res.redirect(307, `${decodeURIComponent(redir as string)}`);
+    }
+
+    if(response.errors) {
+        res.redirect(307, `${decodeURIComponent(redir as string)}`);
+    }
+}
