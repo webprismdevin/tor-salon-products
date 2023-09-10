c95b8203bc84d3c13c761960543af3254c710aaf
Devin Santamaria <devin@webprism.co>
2023-09-09 21:58:13 -0700

    doot

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig = {
-  basePath: ".",
+  basePath: "/",
   reactStrictMode: true,
   images: {
     // need to change first domain to project domain
