023a2369f6c88bfb88094753adbfa057654a7e57
Devin Santamaria <devin@webprism.co>
2023-09-26 21:40:36 -0700

    add Shopify Inbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ThemeProvider from "./chakra-provider";
 import "./globals.css";
 import Banner from "../components/Banner";
 import MailingList from "../components/Global/MailingList";
+import Script from "next/script";
 
 async function getData() {
   const res = await sanity.fetch(settingsQuery);
@@ -47,6 +48,11 @@ export default async function RootLayout({
           </body>
         </CartProvider>
       </ThemeProvider>
+      <Script
+        strategy="lazyOnload"
+        id="shopify-chat-client"
+        src={`https://cdn.shopify.com/shopifycloud/shopify_chat/storefront/shopifyChatV1.js?api_env=production&amp;c=black&amp;i=chat_bubble&amp;p=bottom_right&amp;s=icon&amp;shop_id=GyYJbarWb0rkz6DeTBx77Epcx9tSOCyWSRmR3-AgxX8&amp;t=chat_with_us&amp;v=1&amp;vp=lowest&amp;shop=tor-salon-products.myshopify.com`}
+      />
     </html>
   );
 }
